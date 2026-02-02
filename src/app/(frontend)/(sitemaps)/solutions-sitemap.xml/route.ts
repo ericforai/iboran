ca163ce83e67bf5eb96b9f61ab217621e7984d6e
ericforai <eric.luwenrong@gmail.com>
2026-02-02 08:01:13 +0800

    chore: update sitemap domain to www.iboran.com

--- src/app/(frontend)/(sitemaps)/solutions-sitemap.xml/route.ts
+++ src/app/(frontend)/(sitemaps)/solutions-sitemap.xml/route.ts
@@ -9,7 +9,7 @@ const getSolutionsSitemap = unstable_cache(
     const SITE_URL =
       process.env.NEXT_PUBLIC_SERVER_URL ||
       process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-      'https://example.com'
+      'https://www.iboran.com'
 
     const dateFallback = new Date().toISOString()
 
